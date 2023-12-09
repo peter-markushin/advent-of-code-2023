cc9965e979f80b44444aae1b59b5e943f056fb65
Peter Markushin <piotr-m@piotr-m.net>
2023-12-09 23:39:21 +0300

    Day 8 Task 2

--- day-8/nav.go
+++ day-8/nav.go
@@ -25,6 +25,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	points := make(map[string]Point)
 	instructions := ""
+	cur := make([]string, 0)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -41,31 +42,68 @@ func main() {
 		pt, next, _ := s.Cut(line, " = ")
 		L, R := next[1:4], next[6:9]
 		points[pt] = Point{L: L, R: R}
+
+		if pt[2] == 'A' {
+			cur = append(cur, pt)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	sum := 0
-	cur := "AAA"
-	ptr := 0
+	pathsLen := make([]int, len(cur))
 
-	for cur != "ZZZ" {
-		sum += 1
+	for i, _ := range cur {
+		ptr := 0
 
-		if instructions[ptr] == 'L' {
-			cur = points[cur].L
-		} else {
-			cur = points[cur].R
+		for cur[i][2] != 'Z' {
+			if instructions[ptr] == 'L' {
+				cur[i] = points[cur[i]].L
+			} else {
+				cur[i] = points[cur[i]].R
+			}
+
+			if ptr == len(instructions)-1 {
+				ptr = 0
+			} else {
+				ptr += 1
+			}
+
+			pathsLen[i] += 1
 		}
 
-		if ptr < len(instructions)-1 {
-			ptr += 1
-		} else {
-			ptr = 0
+	}
+
+	sum := pathsLen[0]
+
+	if len(pathsLen) > 1 {
+		for i := 1; i < len(pathsLen); i++ {
+			sum = lcm(sum, pathsLen[i])
 		}
 	}
 
-	fmt.Printf("Sum %d", sum)
+	fmt.Printf("\nSum %d", sum)
+}
+
+func lcm(a, b int) int {
+	if a == 0 && b == 0 {
+		return 0
+	}
+
+	return abs(a*b) / gcd(a, b)
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
